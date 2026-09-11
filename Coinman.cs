277ee672f2dae4580461f500addfcd3f95821d40
1587802918 <130744985+1587802918@users.noreply.github.com>
2026-09-11 12:06:55 +0800

    修改吃豆人移动逻辑，增加撞墙逻辑。

--- Coinman.cs
+++ Coinman.cs
@@ -5,27 +5,27 @@ public partial class Coinman : CharacterBody2D
 {
 	public float StepSize = 80.0f;
 
-	public override void _UnhandledInput(InputEvent @event)
+	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 step = Vector2.Zero;
 
-		if(Input.IsActionPressed("move_right"))
+		if(Input.IsActionJustPressed("move_right"))
 		{
 			step.X = StepSize;
 		}
-		if(Input.IsActionPressed("move_left"))
+		if(Input.IsActionJustPressed("move_left"))
 		{
 			step.X = -StepSize;
 		}
-		if(Input.IsActionPressed("move_up"))
+		if(Input.IsActionJustPressed("move_up"))
 		{
 			step.Y = -StepSize;
 		}
-		if(Input.IsActionPressed("move_down"))
+		if(Input.IsActionJustPressed("move_down"))
 		{
 			step.Y = StepSize;
 		}
-		if(step != Vector2.Zero){
+		if(step != Vector2.Zero && !TestMove(GlobalTransform, step)){
 			Position += step;
 		}
 	}
