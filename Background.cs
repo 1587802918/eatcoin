2f3af43d881d8d26c51bd4687dbe66418b65360c
1587802918 <130744985+1587802918@users.noreply.github.com>
2026-09-15 16:06:24 +0800

    新增crt滤镜，增加画面质感

--- Background.cs
+++ Background.cs
@@ -7,24 +7,27 @@ public partial class Background : Node2D
 {
 	public int GridWidth = 9;
 	public int GridHeight = 16;
-	public Vector2 CellSize = new Vector2(80f,80f);
-	public Color LineColor = new Color(1f,1f,1f,0.4f);
+	public Vector2 CellSize = new Vector2(80f, 80f);
+	public Color LineColor = new Color(1f, 1f, 1f, 0.4f);
 	public float LineThickness = 2.0f;
-	public override void _Draw(){
+	public override void _Draw()
+	{
 		float totalWidth = GridWidth * CellSize.X;
 		float totalHeight = GridHeight * CellSize.Y;
-		for(int i=0;i<=GridWidth;i++){
+		for (int i = 0; i <= GridWidth; i++)
+		{
 			float x = i * CellSize.X;
-			Vector2 from = new Vector2(x,0);
-			Vector2 to = new Vector2(x,totalHeight);
-			DrawLine(from,to,LineColor,LineThickness);
+			Vector2 from = new Vector2(x, 0);
+			Vector2 to = new Vector2(x, totalHeight);
+			DrawLine(from, to, LineColor, LineThickness);
 		}
-		
-		for(int i=0;i<=GridHeight;i++){
+
+		for (int i = 0; i <= GridHeight; i++)
+		{
 			float y = i * CellSize.Y;
-			Vector2 from = new Vector2(0,y);
-			Vector2 to = new Vector2(totalWidth,y);
-			DrawLine(from,to,LineColor,LineThickness);
+			Vector2 from = new Vector2(0, y);
+			Vector2 to = new Vector2(totalWidth, y);
+			DrawLine(from, to, LineColor, LineThickness);
 		}
 	}
 }
