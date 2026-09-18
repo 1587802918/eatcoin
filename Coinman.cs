d67954cb372d413ac4dd8d58810e3d457e526e1a
1587802918 <130744985+1587802918@users.noreply.github.com>
2026-09-18 19:41:27 +0800

    增加滑行

--- Coinman.cs
+++ Coinman.cs
@@ -3,13 +3,22 @@ using System;
 
 public partial class Coinman : CharacterBody2D
 {
-	private AnimatedSprite2D _sprite;
 
 	[Export] public float Speed = 300.0f;
 
 	[Export] public float CellSize = 80.0f;
 
+	[Export] public int DashCells = 5;
+	[Export] public float DashMultiplier = 5.0f;
+
+	private AnimatedSprite2D _sprite;
+
+
 	private Vector2 _moveDirection = Vector2.Zero;
+
+	private float _dashDistance = 0.0f;
+
+	private Vector2 SnappedPosition => (Position - Vector2.One * (CellSize * 0.5f)).Snapped(Vector2.One * CellSize) + Vector2.One * (CellSize * 0.5f);
 
 	public override void _Ready()
 	{
@@ -18,56 +27,61 @@ public partial class Coinman : CharacterBody2D
 
 	public override void _PhysicsProcess(double delta)
 	{
+		if (_dashDistance <= 0f)
+		{
+			Vector2 nextDir = Vector2.Zero;
+			if (Input.IsActionJustPressed("move_right")) nextDir = Vector2.Right;
+			else if (Input.IsActionJustPressed("move_left")) nextDir = Vector2.Left;
+			else if (Input.IsActionJustPressed("move_up")) nextDir = Vector2.Up;
+			else if (Input.IsActionJustPressed("move_down")) nextDir = Vector2.Down;
+			if (nextDir != Vector2.Zero)
+			{
+				_moveDirection = nextDir;
+				if (_moveDirection.X < 0)
+				{
+					_sprite.FlipH = true;
+					_sprite.Rotation = 0;
+				}
+				else
+				{
+					_sprite.FlipH = false;
+					_sprite.Rotation = _moveDirection.Angle();
+				}
+				Position = SnappedPosition;
+			}
 
-		Vector2 nextDir = Vector2.Zero;
-		if (Input.IsActionJustPressed("move_right"))
-		{
-			nextDir = Vector2.Right;
-		}
-		else if (Input.IsActionJustPressed("move_left"))
-		{
-			nextDir = Vector2.Left;
-		}
-		else if (Input.IsActionJustPressed("move_up"))
-		{
-			nextDir = Vector2.Up;
-		}
-		else if (Input.IsActionJustPressed("move_down"))
-		{
-			nextDir = Vector2.Down;
-		}
-		if (nextDir != Vector2.Zero)
-		{
-			_moveDirection = nextDir;
-			UpdateSpriteOrientation(_moveDirection);
-			Position = (Position - Vector2.One * (CellSize * 0.5f)).Snapped(Vector2.One * CellSize) + Vector2.One * (CellSize * 0.5f); ;
+			if (Input.IsActionJustPressed("dash") && _moveDirection != Vector2.Zero)
+			{
+				_dashDistance = DashCells * CellSize;
+
+			}
+
 
 		}
 		if (_moveDirection != Vector2.Zero)
 		{
-			Velocity = _moveDirection * Speed;
+			float currentSpeed = _dashDistance > 0f ? Speed * DashMultiplier : Speed;
+			Velocity = _moveDirection * currentSpeed;
 			MoveAndSlide();
+			if (_dashDistance > 0f)
+			{
+				_dashDistance -= currentSpeed * (float)delta;
+				if (_dashDistance <= 0f)
+				{
+					_dashDistance = 0f;
+					Position = SnappedPosition;
+				}
+			}
+
 			if (GetSlideCollisionCount() > 0)
 			{
+				_dashDistance = 0f;
 				_moveDirection = Vector2.Zero;
 				Velocity = Vector2.Zero;
-				Position = (Position - Vector2.One * (CellSize * 0.5f)).Snapped(Vector2.One * CellSize) + Vector2.One * (CellSize * 0.5f); ;
+				Position = SnappedPosition;
 			}
 		}
 	}
 
-	private void UpdateSpriteOrientation(Vector2 dir)
-	{
-		if (_moveDirection.X < 0)
-		{
-			_sprite.FlipH = true;
-			_sprite.Rotation = 0;
-		}
-		else
-		{
-			_sprite.FlipH = false;
-			_sprite.Rotation = dir.Angle();
-		}
 
-	}
 }
